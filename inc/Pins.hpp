76684a349d789f6790102f315566723819ac83c8
integralfx <16512539+integralfx@users.noreply.github.com>
2025-02-02 09:59:40 +1100

    Organise pins

--- inc/Pins.hpp
+++ inc/Pins.hpp
@@ -1,27 +1,31 @@
 #pragma once
 #define BTN_Pin GPIO_PIN_13
 #define BTN_GPIO_Port GPIOC
-#define LCD_D1_Pin GPIO_PIN_4
-#define LCD_D1_GPIO_Port GPIOB
-#define LCD_E_Pin GPIO_PIN_3
-#define LCD_E_GPIO_Port GPIOB
-#define TEMP_DATA_Pin GPIO_PIN_8
-#define TEMP_DATA_GPIO_Port GPIOB
-#define LCD_D0_Pin GPIO_PIN_5
-#define LCD_D0_GPIO_Port GPIOB
-#define LCD_RS_Pin GPIO_PIN_10
-#define LCD_RS_GPIO_Port GPIOA
-#define LCD_D4_Pin GPIO_PIN_9
-#define LCD_D4_GPIO_Port GPIOA
-#define LCD_D6_Pin GPIO_PIN_6
-#define LCD_D6_GPIO_Port GPIOB
-#define LCD_D3_Pin GPIO_PIN_8
-#define LCD_D3_GPIO_Port GPIOA
-#define LCD_D5_Pin GPIO_PIN_7
-#define LCD_D5_GPIO_Port GPIOC
 #define LED_Pin GPIO_PIN_5
 #define LED_GPIO_Port GPIOA
+// LCD enable pin
+#define LCD_E_Pin GPIO_PIN_3
+#define LCD_E_GPIO_Port GPIOB
+// LCD reset pin
+#define LCD_RS_Pin GPIO_PIN_10
+#define LCD_RS_GPIO_Port GPIOA
+// LCD data pins
+#define LCD_D0_Pin GPIO_PIN_5
+#define LCD_D0_GPIO_Port GPIOB
+#define LCD_D1_Pin GPIO_PIN_4
+#define LCD_D1_GPIO_Port GPIOB
 #define LCD_D2_Pin GPIO_PIN_10
 #define LCD_D2_GPIO_Port GPIOB
+#define LCD_D3_Pin GPIO_PIN_8
+#define LCD_D3_GPIO_Port GPIOA
+#define LCD_D4_Pin GPIO_PIN_9
+#define LCD_D4_GPIO_Port GPIOA
+#define LCD_D5_Pin GPIO_PIN_7
+#define LCD_D5_GPIO_Port GPIOC
+#define LCD_D6_Pin GPIO_PIN_6
+#define LCD_D6_GPIO_Port GPIOB
 #define LCD_D7_Pin GPIO_PIN_7
 #define LCD_D7_GPIO_Port GPIOA
+// Temperature sensor data pin
+#define TEMP_DATA_Pin GPIO_PIN_8
+#define TEMP_DATA_GPIO_Port GPIOB
